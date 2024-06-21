0dbb22ede9c0d5b90f339ba2c089cf0ef1286112
Lee Harrison <61495059+hciharrison@users.noreply.github.com>
2024-06-21 15:00:28 +0100

    Update Set-CSV-Owner.ps1

--- powershell/Set-CSV-Owner.ps1
+++ powershell/Set-CSV-Owner.ps1
@@ -1,12 +1,12 @@
 #Set cluster
-[string]$Cluster = "hci-c1"
+[string]$Cluster = "hci-c01"
 #Get CSV's from cluster
-[string[]]$CSVs = Get-ClusterSharedVolume -Cluster "$Cluster"
+[string[]]$CSVs = Get-ClusterSharedVolume -Cluster "$Cluster" | Where-Object {$_.Name -notlike '*Infrastructure_1*'}
 #Set required owners
-$RequiredOwners = @{"RequiredOwner-Volume01" = "hci-c1-n1";
-                    "RequiredOwner-Volume02" = "hci-c1-n1";
-                    "RequiredOwner-Volume03" = "hci-c1-n2";
-                    "RequiredOwner-Volume04" = "hci-c1-n2";
+$RequiredOwners = @{"RequiredOwner-UserStorage_1" = "hci-c01-n01";
+                    "RequiredOwner-UserStorage_2" = "hci-c01-n02";
+                    "RequiredOwner-UserStorage_3" = "hci-c01-n03";
+                    "RequiredOwner-UserStorage_4" = "hci-c01-n04";
                     }
 
     #Start loop through CSV's
@@ -18,12 +18,12 @@ $RequiredOwners = @{"RequiredOwner-Volume01" = "hci-c1-n1";
     #Get current owner node
     [string]$CurrentOwner = (Get-ClusterSharedVolume -Cluster $Cluster -Name "$CSV").OwnerNode
     #Get Volume number by taking subsrting value away from name
-    $volsubslength = ((((Get-ClusterSharedVolume -Cluster $Cluster -Name "$CSV").SharedVolumeInfo.FriendlyVolumeName) | Split-Path -Leaf).Length) - 2
+    $volsubslength = ((((Get-ClusterSharedVolume -Cluster $Cluster -Name "$CSV").SharedVolumeInfo.FriendlyVolumeName) | Split-Path -Leaf).Length) - 1
     [string]$VolNumber = (((Get-ClusterSharedVolume -Cluster $Cluster -Name "$CSV").SharedVolumeInfo.FriendlyVolumeName) | Split-Path -Leaf).Substring($volsubslength)
 
 
         #Check volume ownership
-        $ReqOwner = $RequiredOwners.Get_Item("RequiredOwner-Volume$VolNumber")
+        $ReqOwner = $RequiredOwners.Get_Item("RequiredOwner-UserStorage_$VolNumber")
         If($CurrentOwner -eq $ReqOwner){
         Write-Host ""$CSV" is already owned by Node $ReqOwner" -ForegroundColor Green
         }
@@ -35,4 +35,5 @@ $RequiredOwners = @{"RequiredOwner-Volume01" = "hci-c1-n1";
         Sleep 1
         }
     }
-    
+
+
